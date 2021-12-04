a03655445e900c62698d2584eff6a9e99e4718c3
truonghoangduy <truonghoangduy03111999@gmail.com>
2021-12-04 21:17:47 +0700

    fix typo

--- lib/di.dart
+++ lib/di.dart
@@ -3,11 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:gateway/blocs/ble_repository.dart';
 
-class ProvideDependency extends StatelessWidget {
+class DependencyProvider extends StatelessWidget {
   final Widget child;
   final FlutterReactiveBle rxBle = FlutterReactiveBle();
 
-  ProvideDependency({Key? key, required this.child}) : super(key: key);
+  DependencyProvider({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
