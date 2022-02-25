da4f42474793ea1e96c6eedf0748fbb950c4a2e7
truonghoangduy <truonghoangduy03111999@gmail.com>
2022-02-25 22:52:25 +0700

    testscreen

---
+++ lib/screens/test/api_call.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:gateway/config/constants/service_constants.dart';
+import 'package:gateway/mock/mock_model.dart';
+import 'package:gateway/services/device_service.dart';
+
+class APICallTest extends StatefulWidget {
+  const APICallTest({Key? key}) : super(key: key);
+
+  @override
+  _APICallTestState createState() => _APICallTestState();
+}
+
+class _APICallTestState extends State<APICallTest> {
+  final service = DeviceService();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      color: Colors.greenAccent,
+      child: Column(
+        children: [
+          const Text(
+              'TEST API ${AppConstantsService.ENDPOINT_VERSION} SECTION'),
+          TextButton(
+              onPressed: () => {service.postDeviceData(mockDeviceSensorData)},
+              child: Text("postDeviceData")),
+          TextButton(
+              onPressed: () =>
+                  {service.postDeviceDetail(mockDeviceHardWareInfo)},
+              child: Text("device Claim"))
+        ],
+      ),
+    );
+  }
+}
