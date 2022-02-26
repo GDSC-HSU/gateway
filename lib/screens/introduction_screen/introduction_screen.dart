9b34dfe0e68be50100ce2a09ac23ac3c60462c4c
HUNG.NM5173 2175173 <HUNG.NM5173@SINHVIEN.HOASEN.EDU.VN>
2022-02-26 16:22:03 +0700

    add gif

--- lib/screens/introduction_screen/introduction_screen.dart
+++ lib/screens/introduction_screen/introduction_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gateway/config/themes/gateway_color.dart';
 
 class IntroductionScreen extends StatefulWidget {
@@ -12,21 +13,46 @@ class _IntroductionScreenState extends State<IntroductionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        children: [
-          Container(
-            color: GatewayColors.scaffoldBgLight,
-            child: Center(child: Text('hello')),
-          ),
-          Container(
-            color: GatewayColors.scaffoldBgLight,
-            child: Center(child: Text('hellooooooooooooooo')),
-          ),
-          Container(
-            color: GatewayColors.scaffoldBgLight,
-            child: Center(child: Text('hellooooooo')),
-          ),
-        ],
+      body: Container(
+        padding: EdgeInsets.only(bottom: 140.h),
+        child: PageView(
+          children: [
+            Container(
+              color: GatewayColors.buttonBgLight,
+              child: Center(child: Text('hello')),
+            ),
+            Container(
+              color: GatewayColors.buttonBgLight,
+              child: Center(child: Text('hellooooooooooooooo')),
+            ),
+            Container(
+              color: GatewayColors.buttonBgLight,
+              child: Center(child: Text('hellooooooo')),
+            ),
+          ],
+        ),
+      ),
+      bottomSheet: Container(
+        height: 140.h,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 47.h,
+              height: 47.h,
+              decoration: BoxDecoration(
+                color: GatewayColors.buttonBgLight,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
