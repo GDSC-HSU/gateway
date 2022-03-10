cd0241b19c7510ea15cb112d06bdabb42165cac6
truonghoangduy <truonghoangduy03111999@gmail.com>
2022-03-10 23:42:59 +0700

    style: :rotating_light: namming

--- lib/config/themes/gateway_color.dart
+++ lib/config/themes/gateway_color.dart
@@ -5,7 +5,7 @@ class GatewayColors {
   static const Color buttonBgLight = Color(0xFF3967D6);
   static const Color textDefaultBgLight = Color(0xFFA1A4B9);
   static const Color textPermissionBgLight = Color(0xFF606060);
-  static const Color disablebuttonBgLight = Color(0xFF9C9C9C);
+  static const Color disableButtonBgLight = Color(0xFF9C9C9C);
   static const Color dividerColorLight = Color(0xFFF2F2F2);
   static const Color bgButtonColorHaveBorder = Color(0xFFDDE7FF);
   static const Color outLineCheckBox = Color(0xFFE4E4E4);
