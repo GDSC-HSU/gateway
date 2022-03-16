5c5e59afca3928d0a572eea69e75a0c97b5c499e
truonghoangduy <truonghoangduy03111999@gmail.com>
2022-03-17 00:16:12 +0700

    add screen dependency

--- lib/config/routes/router_name.dart
+++ lib/config/routes/router_name.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:gateway/config/routes/routing.dart';
+import 'package:gateway/di/di_ble_sensor.dart';
 import 'package:gateway/screens/congratulation_screen/congratulation_screen.dart';
 import 'package:gateway/screens/gateway_check_screen/gateway_check_screen.dart';
 import 'package:gateway/screens/introduction_screen/introduction_screen.dart';
@@ -8,8 +9,6 @@ import 'package:gateway/screens/setup_screen/setup_screen.dart';
 import 'package:gateway/widgets/confirm/confirm_screen.dart';
 import 'package:gateway/widgets/connect/connect_screen.dart';
 import 'package:gateway/widgets/scan/scan_screen.dart';
-
-
 
 class RouterName {
   late List<CameraDescription> cameras;
@@ -26,7 +25,9 @@ class RouterName {
       case AppRouting.setup:
         return MaterialPageRoute(builder: (context) => SetupScreen());
       case AppRouting.gatewayCheck:
-        return MaterialPageRoute(builder: (context) => GatewayCheckScreen());
+        return MaterialPageRoute(
+            builder: (context) =>
+                AppBLESensorDependencyProvider(child: GatewayCheckScreen()));
       case AppRouting.congratulation:
         return MaterialPageRoute(builder: (context) => CongratulationScreen());
       default:
